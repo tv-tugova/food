a49bc74b1d2d3f6820dc050153c13bbc33b1d5e0
Tatiana <tvtugova1511@gmail.com>
2024-03-08 12:24:46 +0300

    Установлен json-server

--- js/script.js
+++ js/script.js
@@ -284,4 +284,9 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
